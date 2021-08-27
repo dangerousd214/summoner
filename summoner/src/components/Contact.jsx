10703a2e5ae114f745968519e94ad0761808bcdb
Darren Watson <v_tec@live.com>
2021-08-27 12:24:16 -0500

    refractored existing website to work in react

---
+++ summoner/src/components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div class="container">
+          <div class="d-block p-5 h-50">
+            <img
+              class="img-fluid rounded "
+              src="../Summoner-Timeline.png"
+              alt="release timeline"
+            />
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ summoner/src/components/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="../logo.png"
+              alt="Summoner Logo"
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Welcome to Summoner</h1>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
